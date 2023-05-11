7be44c38f1b2e56bd5bdc58ce2cdb6a2ecd69328
Jerson Mamangun <117369344+Shiverooo@users.noreply.github.com>
2023-05-11 23:24:40 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// DROPDOWN MENU
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+}
+
+// IMAGE SLIDER AT HOME PAGE
+const myslide = document.querySelectorAll('.myslider'),
+    dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 4000);
+function autoSlide() {
+    counter += 1;
+    slidefun(counter);
+}
+function plusSlides(n) {
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
+function currentSlide(n) {
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(autoSlide, 4000);
+}
+
+function slidefun(n) {
+
+    let i;
+    for (i = 0; i < myslide.length; i++) {
+        myslide[i].style.display = "none";
+    }
+    for (i = 0; i < dot.length; i++) {
+        dot[i].classList.remove('active');
+    }
+    if (n > myslide.length) {
+        counter = 1;
+    }
+    if (n < 1) {
+        counter = myslide.length;
+    }
+    myslide[counter - 1].style.display = "block";
+    dot[counter - 1].className += " active";
+}
+
+
